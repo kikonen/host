a1d8334a5193def699551ad4c8469e09bd1c596c
kari <mr.kari.ikonen@gmail.com>
2015-03-26 23:11:22 +0200

    LF: Added themes - ADD: cerulean basic theme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :require_theme
+
+  def require_theme
+    @theme_id = 'cerulean'
+  end
 end
