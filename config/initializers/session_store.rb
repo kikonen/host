ef43f2da1c4d2e4966f730bb32a61cc415561967
kari <mr.kari.ikonen@gmail.com>
2016-07-09 12:43:39 +0300

    use memcached as session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_host_session'
+#Rails.application.config.session_store :cookie_store, key: '_host_session'
+Rails.application.config.session_store ActionDispatch::Session::CacheStore,
+  expire_after: 20.minutes,
+  key: '_gi_session_id'
