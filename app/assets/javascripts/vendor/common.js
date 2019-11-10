59139538d2aad5d95551c5af724ec2b7aa4a555f
Kari Ikonen <mr.kari.ikonen@gmail.com>
2019-11-10 18:19:36 +0200

    FIX moment 2.24.0

--- app/assets/javascripts/vendor/common.js
+++ app/assets/javascripts/vendor/common.js
@@ -1,9 +1,9 @@
 //
 //= require lodash-4.17.5/lodash
 //
-//= require moment-2.21.0/moment
-//= require moment-2.21.0/locale/fi
-//= require moment-timezone-0.5.14/moment-timezone
+//= require moment-2.24.0/moment
+//= require moment-2.24.0/locale/fi
+//= require moment-timezone-0.5.27/moment-timezone
 //
 //= require jquery-3.3.1/jquery
 //= require jquery-ujs-1.0.3/rails
