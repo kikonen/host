bea7cf3c903c8053bad8fe5716363877968da932
Kari Ikonen <mr.kari.ikonen@gmail.com>
2019-11-10 15:56:17 +0200

    FIX upgrade to 6.0 defaults FIX updated to puma in dev env

--- config/boot.rb
+++ config/boot.rb
@@ -1,24 +1,39 @@
+# frozen_string_literal: true
+
+Thread.current.name = 'INIT' unless Thread.current.name
+
+# config/boot.rb
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
-# No stdout for logger
-require 'rack'
+# NOTE KI bootsnap crashes with segmentation fault
+#require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+# @see https://github.com/rails/rails/issues/28968#issuecomment-326437354
 require 'rails/command'
 require 'rails/commands/server/server_command'
 
+# No stdout for logger
 module Rails
   class Server < ::Rack::Server
-    alias_method :orig_start, :start
-
-    def start
-      orig_start
-      options[:log_stdout] = false
+    alias_method :orig_initialize, :initialize
+    def initialize(options)
+      orig_initialize(options.merge(log_stdout: false, AccessLog: []))
     end
+  end
+end
 
-    private
-    def log_to_stdout
-      # No stdout for logger
+#
+# HACK KI
+# ActiveRecord::Railtie starts outputting to STDERR via "rails c"; forbid such malware
+#
+module ActiveSupport
+  class Logger < ::Logger
+    def self.logger_outputs_to?(logger, *sources)
+      logdev = logger.instance_variable_get("@logdev")
+      logger_source = logdev.dev if logdev.respond_to?(:dev)
+      sources.any? { |source| source == logger_source || source == STDERR || source == STDOUT }
     end
   end
 end
