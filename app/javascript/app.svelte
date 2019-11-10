56975aad4fd1a100f505ddab7be59460e095d8c6
Kari Ikonen <mr.kari.ikonen@gmail.com>
2019-11-10 23:36:15 +0200

    FIX experiment event handler

--- app/javascript/app.svelte
+++ app/javascript/app.svelte
@@ -1,12 +1,19 @@
 <script>
-  export let name;
-  export let message;
+ export let counter = 0;
+ export let name;
+ export let message;
+
+ function onClickCounter() {
+     counter = counter + 1;
+ }
 </script>
 
 <style>
-  h1 {
-    color: #FF3E00;
-  }
+ h1 {
+     color: #FF3E00;
+ }
 </style>
 
-<h1>Hello me too {name} via {message}!</h1>
+<h1>Hello me too {name} via {message} - {counter}!</h1>
+
+<button on:click="{onClickCounter}">Click me</button>
