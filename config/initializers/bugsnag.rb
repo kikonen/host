bed22a91305afd01c5bbff4ca567715e4ae8e646
Kari Ikonen <mr.kari.ikonen@gmail.com>
2020-01-16 22:57:32 +0200

    FIX select with scss styling

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,3 +1,6 @@
 Bugsnag.configure do |config|
   config.api_key = "dda6ce15dd173fb8dab0f78238662b95"
+  config.notify_release_stages = ['production']
+#  config.notify_release_stages = ['production', 'development']
+
 end
