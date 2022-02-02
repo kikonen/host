492ecaedd97f7312f42fd2fa44f392a94ec37bca
Kari Ikonen <mr.kari.ikonen@gmail.com>
2022-02-02 13:03:58 +0200

    FIX moved docker to demo_env

--- docker/docker-entrypoint-production.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo chown -R docker:users /bundle /node_modules /app/log /app/tmp /app/public
-
-# NOTE KI *NEW* intance clear old trash
-rm /app/tmp/pids/server.pid
-
-bundle exec rails s -b 0.0.0.0 -p 3000
--- docker/server.sh
+++
@@ -1,7 +0,0 @@
-DIR=`dirname $0`
-DIR=`realpath $DIR`
-ROOT_DIR=`dirname $DIR`
-DOCKER_DIR=`realpath "$ROOT_DIR/../.."`
-CONTAINER=host
-
-$DOCKER_DIR/scripts/run_server.sh $CONTAINER "$@"
